8dfb6bc16ec7f28d3ba6992a9247ce3b46d0ab26
Veronika <vee66veen@gmail.com>
2026-07-20 22:42:00 +0200

    .

--- parser.py
+++ parser.py
@@ -1,12 +1,15 @@
-from zone import Zone
-
 
 class Parser:
     def __init__(self, filepath: str) -> None:
         self.filepath = filepath
-        self.nb_drones = 0
 
-    def parse(self) -> None:
+        self.data = {
+            "nb_drones": None,
+            "zones": [],
+            "connections": [],
+        }
+
+    def parse(self) -> dict:
         with open(self.filepath, "r") as file:
             for line_number, line in enumerate(file, start=1):
                 line = line.strip()
@@ -15,41 +18,70 @@ class Parser:
                     continue
 
                 self.parse_line(line, line_number)
+        return self.data
 
     def parse_line(self, line: str, line_number: int) -> None:
         if line.startswith("nb_drones:"):
-            self.parse_nb_drones(line)
+            self.parse_nb_drones(line, line_number)
         elif line.startswith("start_hub:"):
-            self.parse_zone(line)
+            self.parse_zone(line, line_number)
         elif line.startswith("end_hub:"):
-            self.parse_zone(line)
+            self.parse_zone(line, line_number)
         elif line.startswith("hub:"):
-            self.parse_zone(line)
+            self.parse_zone(line, line_number)
         elif line.startswith("connection:"):
-            self.parse_connection(line)
+            self.parse_connection(line, line_number)
         else:
             raise ValueError(
                 f"Line {line_number}: unknown line type"
             )
 
-    def parse_nb_drones(self, line: str) -> None:
+    def parse_nb_drones(self, line: str, line_number: int) -> None:
         part = line.split()
-        if part < 2:
+
+        if len(part) < 2:
             raise ValueError(
-                "Missing number of drones"
+                f"Line {line_number}: Missing number of drones"
             )
-        elif part > 2:
+
+        elif len(part) > 2:
             raise ValueError(
-                "Too many drone arguments"
+                f"Line {line_number}: Too many drone arguments"
             )
-        self.nb_drones = int(part[1])
+        try:
+            self.data["nb_drones"] = int(part[1])
+        except ValueError:
+            raise ValueError(
+                f"Line {line_number}: number of drones must be an integer"
+            )
 
-    def parse_zone(self, line: str) -> None:
+    def parse_zone(self, line: str, line_number: int) -> None:
+        part = line.split()
 
-    def parse_connection(self, line: str) -> None:
-        print("niuushdi")
+        if len(part) < 4:
+            raise ValueError(
+                f"Line {line_number}: Missing zone arguments"
+            )
+        zone_type = part[0].removesuffix(":")
+        metadata_arg = " ".join(part[4:])
 
-        
+        zone_data = {
+            "type": zone_type,
+            "name": part[1],
+            "x": part[2],
+            "y": part[3],
+            "metadata": metadata_arg,
+            "line_number": line_number,
+        }
+
+        self.data["zones"].append(zone_data)
+
+    def parse_connection(self, line: str, line_number: int) -> None:
+        part = line.split()
+        splitted_connections = part[1].split("-")
+        from_zone = splitted_connections[0]
+        to_zone = splitted_connections[1]
+        self.data["connections"].append()
 
 # zone = Zone(
 #             name=part[1],
