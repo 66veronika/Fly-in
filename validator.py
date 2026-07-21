06d22f333d337684682c8a3bf4a2134e8a6b9d81
Veronika <vee66veen@gmail.com>
2026-07-21 20:26:15 +0200

    .

--- validator.py
+++ validator.py
@@ -13,4 +13,18 @@ class Validator:
     def validate_nb_drones() -> None:
         nb_drones = self.data["nb_drones"]
 
-        if nb_drones
+        if nb_drones is None:
+            raise ValueError(
+                "nb_drones is missing"
+            )
+        if nb_drones <= 0:
+            raise ValueError(
+                "Number of drones must be a positive integer"
+            )
+    def validate_zones(self) -> None:
+        zones = self.data["zones"]
+        name = set()
+        start_hub = 0
+        end_hub = 0
+
+        
